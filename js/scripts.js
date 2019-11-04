9510a10fb7e274c21ca2d6fbc76f3a4ad692e876
Student <example@example.com>
2019-11-04 11:39:39 -0800

    add li tags to show user's input of location and landmark

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,6 @@ Travel.prototype.addDestination = function(destination) {
 
 
 
-
 //Business Logic for individual destinations
 function Destination(location, landmarks, time, notes){
   this.location = location,
@@ -17,9 +16,6 @@ function Destination(location, landmarks, time, notes){
   this.notes = notes
 }
 
-var peru = new Destination('Peru', 'Macchu Picchu', '2017', 'Super Awesome');
-var thailand = new Destination('Thailand', 'Wat Po', '2001', 'Such good food!');
-var kansas = new Destination ('Kansas', 'The first Pizza Hut', '2018', 'Not much to see, but really good if you want to watch a movie for cheap');
 
 
 
@@ -37,20 +33,17 @@ $(document).ready(function (){
     console.log(locationInput, landmarkInput, timeInput, notesInput);
 
     var myTravel = new Travel();
+
     var destination1 = new Destination(locationInput, landmarkInput, timeInput, notesInput);
+
+    myTravel.addDestination(destination1);
+    $('.addResult').append('<li>' + myTravel.destinations[0].location + '</li>' + '<li>' + myTravel.destinations[0].landmarks + '</li>');
+    $(".output").show();
 
     console.log(destination1);
 
-    myTravel.addDestination(destination1);
     console.log(myTravel);
-    $('ul').append(myTravel)
-    $(".clickable").click(function(event){
-      $('#peru').text(myTravel).toggle();
 
-      // event.preventDefault();
-    });
+
   });
-
-
-
 });
