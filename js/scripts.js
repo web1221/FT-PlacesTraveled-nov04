278bd6044c155e51939441a87f5bb35fe11fdb6e
Student <example@example.com>
2019-11-04 10:59:12 -0800

    add submit function in HTML and JS/jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,23 @@ function Destination(location, landmarks, time, notes){
 var peru = new Destination('Peru', 'Macchu Picchu', '2017', 'Super Awesome');
 var thailand = new Destination('Thailand', 'Wat Po', '2001', 'Such good food!');
 var kansas = new Destination ('Kansas', 'The first Pizza Hut', '2018', 'Not much to see, but really good if you want to watch a movie for cheap');
+
+
+//user interface Logic
+$(document).ready(function (){
+  $('form#form').submit(function(event){
+    event.preventDefault();
+    var locationInput = $('input#locationInput').val();
+    var landmarkInput = $('input#landmarkInput').val();
+    var timeInput = $('input#timeInput').val();
+    var notesInput = $('input#notes').val();
+  })
+  $(".clickable").click(function(event){
+    $('#peru').text(peru).toggle();
+
+    event.preventDefault();
+  })
+
+
+
+});
